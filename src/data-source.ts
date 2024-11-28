3e3e47ccefbca3abaaea698dc4d001a9cc3964fc
Manuela Antunes <manuela.antunes@lighthouseit.com.br>
2024-11-28 09:38:59 -0300

    config

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
 
-dotenv.config();
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+dotenv.config({ path: envFile });
 
 export const appDataSource = new DataSource({
     type: "postgres",
