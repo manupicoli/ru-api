ece7da1ccbbbf117e04eeb19999ab3f2d3f74309
Manuela Antunes <manuelaantunes004@gmail.com>
2024-11-28 10:48:20 -0300

    fix config

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,11 +7,15 @@ dotenv.config({ path: envFile });
 
 export const appDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432'),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    ...(process.env.NODE_ENV === "production"
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT || "5432"),
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            }),
     synchronize: true,
     logging: false,
     entities: [__dirname+"/entities/*.{js,ts}"],
