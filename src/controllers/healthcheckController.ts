d61c933d82547d9e7ba06d056e6eddbfcb98421d
Manuela Antunes <manuela.antunes@lighthouseit.com.br>
2024-11-27 23:40:23 -0300

    adding info to healthcheck

--- src/controllers/healthcheckController.ts
+++ src/controllers/healthcheckController.ts
@@ -3,10 +3,23 @@ import { appDataSource } from "../data-source";
 
 export class HealthcheckController {
     get = async (req: Request, res: Response) => {
+        const start = process.hrtime();
         try {
             if(appDataSource.isInitialized) {
                 await appDataSource.query('SELECT 1');
-                return res.status(200).json({ message: "Sucesso" });
+                const uptime = process.uptime();
+                const memoryUsage = process.memoryUsage();
+                const cpuUsage = process.cpuUsage();
+                const responseTime = process.hrtime(start);
+
+                return res.status(200).json({ 
+                    message: "Sucesso",
+                    uptime: uptime,
+                    memoryUsage: memoryUsage,
+                    cpuUsage: cpuUsage,
+                    responseTime: responseTime,
+                    timestamp: new Date().toISOString()
+                });
             }
             return res.status(500).json({ message: "Erro" });
         } catch (error) {
